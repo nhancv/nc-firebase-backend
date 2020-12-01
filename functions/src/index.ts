3dccdb114cbacc362ceac8bcb360a2e70333c97c
Nhan Cao <nhancv92@gmail.com>
2020-12-01 14:35:24 +0700

    feat: correct response cloud function to mobile

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,5 +9,5 @@ admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  response.send({data: "Hello from Firebase!", error: ""});
 });
